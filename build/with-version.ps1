32331e646af371c11930a65f7a5c4f87c811c833
masaxsuzu <masaxsuzu+github@gmail.com>
2020-02-13 01:22:55 +0900

    Create release.yml

--- build/with-version.ps1
+++ build/with-version.ps1
@@ -1,15 +1,23 @@
 $ErrorActionPreference = "Stop"
 
-$info = Get-Content .\Product.json | ConvertFrom-Json
-$version = $info.version
-$build = $info.build
-$Title = $info.title
-$company = $info.company
-$product = $info.name
+try {
+    Push-Location  $PSScriptRoot
+    
+    $info = Get-Content ..\Product.json | ConvertFrom-Json
+    $version = $info.version
+    $build = $info.build
+    $Title = $info.title
+    $company = $info.company
+    $product = $info.name
 
-msbuild /t:ReBuild               `
-    /p:Configuration=Release     `
-    /p:Version="$version.$build" `
-    /p:Title=$Title              `
-    /p:Company=$company          `
-    /p:Product=$product
+    msbuild /t:ReBuild               `
+        /p:Configuration=Release     `
+        /p:Version="$version.$build" `
+        /p:Title=$Title              `
+        /p:Company=$company          `
+        /p:Product=$product          `
+        ..\myget.sln
+}
+finally {
+    Pop-Location
+}
